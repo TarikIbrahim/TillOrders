d9d691a7f05558bfff54685f967fd9f27c13cbec
NightCoder <tarikibrahim1982@gmail.com>
2019-10-27 05:18:48 +0200

    no message

---
+++ TillOrders.WebApi/MappingExtension/OrderDtoMappings.cs
@@ -0,0 +1,19 @@
+﻿using TillOrders.Domain.Model;
+using TillOrders.WebApi.AutoMapper;
+using TillOrders.WebApi.Dtos.Order;
+
+namespace TillOrders.WebApi.MappingExtension
+{
+    public static class OrderDtoMappings
+    {
+        public static OrderDto ToDto(this Order order)
+        {
+            return order.MapTo<Order, OrderDto>();
+        }
+
+        public static Order ToEntity(this OrderDto orderDto)
+        {
+            return orderDto.MapTo<OrderDto, Order>();
+        }
+    }
+}
